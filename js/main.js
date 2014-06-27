08457e4c64e70cead4c4255c62558c0d2fced7c8
David Chang <davidc@hasoffers.com>
2014-06-27 10:11:16 -0700

    adding a bunch of changes for normalized comparisons

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
-var app = angular.module('myApp', ['ngGrid'])
+var app = angular.module('myApp', ['ngRoute', 'ngGrid'])
 
-  .controller('MainCtrl', function($scope, KDStatsService) {
-    $scope.gridData = KDStatsService.getStats();
+  .config(function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'views/home.html'
+      }).
+      when('/main', {
+        templateUrl: 'views/mainTemplate.html',
+        controller: 'MainCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  })
+
+  .controller('MainCtrl', function($scope, $location, KDStatsService) {
+
+    $scope.gridData = KDStatsService.getStats($location.hash());
+
     $scope.gridOptions = {
       data: 'gridData',
-      columnDefs: [
-        {field:'date', displayName:'Date'},
-        {field:'opponent', displayName:'Opponent'},
-        {field:'location', displayName:'Location'},
-        {field:'score', displayName:'Score'},
-        {field:'min', displayName:'Minutes'},
-        {field:'fgm_fga', displayName:'FGM-FGA'},
-        {field:'fg_percentage', displayName:'FG%'},
-        {field:'_3pm_3pa', displayName:'3PM-3PA'},
-        {field:'_3p_percentage', displayName:'3P%'},
-        {field:'ftm_fta', displayName:'FTM-FTA'},
-        {field:'ft_percentage', displayName:'FT%'},
-        {field:'rebounds', displayName:'Rebounds'},
-        {field:'assists', displayName:'Assists'},
-        {field:'blocks', displayName:'Blocks'},
-        {field:'steals', displayName:'Steals'},
-        {field:'pf', displayName:'Fouls'},
-        {field:'to', displayName:'Turnovers'},
-        {field:'pts', displayName:'Points'},
-      ]
+      columnDefs: columnDefs
     };
   })
 
