2e400dfaa1d1848b1b1725618cc7f955a039e912
David Chang <davidc@hasoffers.com>
2014-06-29 21:03:08 -0700

    small and large datasets

--- js/main.js
+++ js/main.js
@@ -4,9 +4,13 @@ var app = angular.module('myApp', ['ngRoute', 'ngReactGrid'])
       when('/', {
         templateUrl: 'views/home.html'
       }).
-      when('/main', {
-        templateUrl: 'views/mainTemplate.html',
-        controller: 'MainCtrl'
+      when('/small', {
+        templateUrl: 'views/smallTemplate.html',
+        controller: 'SmallDataCtrl'
+      }).
+      when('/large', {
+        templateUrl: 'views/largeTemplate.html',
+        controller: 'LargeDataCtrl'
       }).
       otherwise({
         redirectTo: '/'
@@ -15,32 +19,69 @@ var app = angular.module('myApp', ['ngRoute', 'ngReactGrid'])
 
   .run(function($templateCache) {
 
-    var grid = new ngReactGridDefaults();
-    grid.data = stats;
-    grid.columnDefs = columnDefs;
-    grid.pageSize = 1000;
-    grid.pageSizes = [1000];
-    grid.totalCount = 640;
-    grid.react.showingRecords = 640;
-    grid.react.startIndex = 0;
-    grid.react.endIndex = 1000;
-    grid.totalPages = 1;
+    var getGrid = function(small) {
+      // all of these are a little lame, but we're basically doing manual calculations/settings
+      // to ensure the below properties will match the initial set of properties sent to the
+      // React component on the server
+      var grid = new ngReactGridDefaults();
+      grid.columnDefs = columnDefs;
+      grid.pageSize = 1000;
+      grid.pageSizes = [1000];
 
-    var viewMarkup = React.renderComponentToString(
-      ngReactGridComponent({ grid : grid })
+      if (!small) {
+        grid.data = stats;
+        grid.totalCount = 640;
+        grid.react.showingRecords = 640;
+      } else {
+        grid.totalCount = 103;
+        grid.react.showingRecords = 103;
+        var toUse = [];
+        stats.forEach(function(stat) {
+          if (stat.date.split('/')[2] == '2014') {
+            toUse.push(stat);
+          }
+        });
+        grid.data = toUse;
+      }
+
+      grid.react.startIndex = 0;
+      grid.react.endIndex = 1000;
+      grid.totalPages = 1;
+
+      return grid;
+    };
+
+    var smallViewMarkup = React.renderComponentToString(
+      ngReactGridComponent({ grid : getGrid(true) })
     );
 
-    viewMarkup = '<div id="gridContainer">' + viewMarkup + '</div>';
+    var largeViewMarkup = React.renderComponentToString(
+      ngReactGridComponent({ grid : getGrid(false) })
+    );
 
-    $templateCache.put('views/mainTemplate.html', viewMarkup);
+    $templateCache.put('views/smallTemplate.html', '<div id="gridContainer">' + smallViewMarkup + '</div>');
+    $templateCache.put('views/largeTemplate.html', '<div id="gridContainer">' + largeViewMarkup + '</div>');
   })
 
-  .controller('MainCtrl', function($scope, KDStatsService, ngReactGrid) {
+  .controller('SmallDataCtrl', function($scope, KDStatsService, ngReactGrid) {
+
     $scope.grid = {
-      data       : KDStatsService.getStats(),
-      columnDefs : columnDefs,
-      pageSize   : 1000,
-      pageSizes  : [1000]
+      data: KDStatsService.getSmallStats(),
+      columnDefs: columnDefs,
+      pageSize : 1000,
+      pageSizes : [1000]
+    };
+
+    new ngReactGrid($scope, angular.element(document.getElementById('gridContainer')));
+  })
+
+  .controller('LargeDataCtrl', function($scope, KDStatsService, ngReactGrid) {
+
+    $scope.grid = {
+      data: KDStatsService.getLargeStats(),
+      columnDefs: columnDefs,
+      pageSize : 1000,
+      pageSizes : [1000]
     };
 
     new ngReactGrid($scope, angular.element(document.getElementById('gridContainer')));
@@ -48,7 +89,16 @@ var app = angular.module('myApp', ['ngRoute', 'ngReactGrid'])
 
   .service('KDStatsService', function() {
     return {
-      getStats : function() {
+      getSmallStats : function() {
+        var toReturn = [];
+        stats.forEach(function(stat) {
+          if (stat.date.split('/')[2] == '2014') {
+            toReturn.push(stat);
+          }
+        });
+        return toReturn;
+      },
+      getLargeStats : function() {
         return stats;
       }
     };
