5387ed4260130a6791a9ff89965de8afd4016fea
David Chang <davidc@hasoffers.com>
2014-06-29 19:26:52 -0700

    adding small, large datasets

--- js/main.js
+++ js/main.js
@@ -5,39 +5,36 @@ var app = angular.module('myApp', ['ngRoute', 'ngReactGrid'])
       when('/', {
         templateUrl: 'views/home.html'
       }).
-      when('/main', {
+      when('/small', {
         templateUrl: 'views/mainTemplate.html',
-        controller: 'MainCtrl'
+        controller: 'SmallDataCtrl'
+      }).
+      when('/large', {
+        templateUrl: 'views/mainTemplate.html',
+        controller: 'LargeDataCtrl'
       }).
       otherwise({
         redirectTo: '/'
       });
   })
 
-  .controller('MainCtrl', function($scope, KDStatsService, ngReactGrid) {
+  .controller('SmallDataCtrl', function($scope, KDStatsService, ngReactGrid) {
 
     $scope.grid = {
-      data: KDStatsService.getStats(),
-      columnDefs: [
-        {field:'date', displayName:'Date'},
-        {field:'opponent', displayName:'Opponent'},
-        {field:'location', displayName:'Location'},
-        {field:'score', displayName:'Score'},
-        {field:'min', displayName:'Minutes'},
-        {field:'fgm_fga', displayName:'FGM-FGA'},
-        {field:'fg_percentage', displayName:'FG%'},
-        {field:'_3pm_3pa', displayName:'3PM-3PA'},
-        {field:'_3p_percentage', displayName:'3P%'},
-        {field:'ftm_fta', displayName:'FTM-FTA'},
-        {field:'ft_percentage', displayName:'FT%'},
-        {field:'rebounds', displayName:'Rebounds'},
-        {field:'assists', displayName:'Assists'},
-        {field:'blocks', displayName:'Blocks'},
-        {field:'steals', displayName:'Steals'},
-        {field:'pf', displayName:'Fouls'},
-        {field:'to', displayName:'Turnovers'},
-        {field:'pts', displayName:'Points'},
-      ],
+      data: KDStatsService.getSmallStats(),
+      columnDefs: columnDefs,
+      pageSize : 1000,
+      pageSizes : [1000]
+    };
+
+    new ngReactGrid($scope, angular.element(document.getElementById('gridContainer')));
+  })
+
+  .controller('LargeDataCtrl', function($scope, KDStatsService, ngReactGrid) {
+
+    $scope.grid = {
+      data: KDStatsService.getLargeStats(),
+      columnDefs: columnDefs,
       pageSize : 1000,
       pageSizes : [1000]
     };
@@ -47,7 +44,16 @@ var app = angular.module('myApp', ['ngRoute', 'ngReactGrid'])
 
   .service('KDStatsService', function() {
     return {
-      getStats : function() {
+      getSmallStats : function() {
+        var toReturn = [];
+        stats.forEach(function(stat) {
+          if (stat.date.split('/')[2] == '2014') {
+            toReturn.push(stat);
+          }
+        });
+        return toReturn;
+      },
+      getLargeStats : function() {
         return stats;
       }
     };
